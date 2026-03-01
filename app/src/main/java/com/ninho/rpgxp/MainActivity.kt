3420e3e4750aa518e034a98ee397fa4043e47697
Ninhoplayer6gg <filhofilho281@gmail.com>
2026-03-01 16:56:39 -0300

    Update MainActivity.kt

--- app/src/main/java/com/ninho/rpgxp/MainActivity.kt
+++ app/src/main/java/com/ninho/rpgxp/MainActivity.kt
@@ -1,38 +1,15 @@
 package com.ninho.rpgxp
 
-import android.opengl.GLSurfaceView
 import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var glView: GLSurfaceView
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        try {
-            glView = GLSurfaceView(this)
-            glView.setEGLContextClientVersion(2)
-            glView.setRenderer(SimpleRenderer())
-
-            setContentView(glView)
-
-        } catch (e: Exception) {
-            val textView = TextView(this)
-            textView.text = e.toString()
-            setContentView(textView)
-        }
-    }
-
-    override fun onPause() {
-        super.onPause()
-        glView.onPause()
-    }
-
-    override fun onResume() {
-        super.onResume()
-        glView.onResume()
+        val textView = TextView(this)
+        textView.text = "APP FUNCIONANDO"
+        setContentView(textView)
     }
 }
