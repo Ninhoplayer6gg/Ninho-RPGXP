1da61a5caab0a13eda7f454b46cc5aa08c812263
Ninhoplayer6gg <filhofilho281@gmail.com>
2026-03-01 15:21:27 -0300

    Update MainActivity.kt

--- app/src/main/java/com/ninho/rpgxp/MainActivity.kt
+++ app/src/main/java/com/ninho/rpgxp/MainActivity.kt
@@ -1,15 +1,30 @@
 package com.ninho.rpgxp
 
 import android.app.Activity
+import android.opengl.GLSurfaceView
 import android.os.Bundle
 
 class MainActivity : Activity() {
 
-    init {
-        System.loadLibrary("native-lib")
-    }
+    private lateinit var glView: GLSurfaceView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        glView = GLSurfaceView(this)
+        glView.setEGLContextClientVersion(3)
+        glView.setRenderer(SimpleRenderer())
+
+        setContentView(glView)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        glView.onPause()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        glView.onResume()
     }
 }
