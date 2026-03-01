c10c0bf0be8c806ab8d518288bae0582c2b80f4d
Ninhoplayer6gg <filhofilho281@gmail.com>
2026-03-01 16:31:59 -0300

    Update MainActivity.kt

--- app/src/main/java/com/ninho/rpgxp/MainActivity.kt
+++ app/src/main/java/com/ninho/rpgxp/MainActivity.kt
@@ -2,26 +2,37 @@ package com.ninho.rpgxp
 
 import android.opengl.GLSurfaceView
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var glView: GLSurfaceView
 
-override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
 
-    try {
+        try {
+            glView = GLSurfaceView(this)
+            glView.setEGLContextClientVersion(2)
+            glView.setRenderer(SimpleRenderer())
 
-        val glView = GLSurfaceView(this)
-        glView.setEGLContextClientVersion(2)
-        glView.setRenderer(SimpleRenderer())
+            setContentView(glView)
 
-        setContentView(glView)
+        } catch (e: Exception) {
+            val textView = TextView(this)
+            textView.text = e.toString()
+            setContentView(textView)
+        }
+    }
 
-    } catch (e: Exception) {
-        val textView = TextView(this)
-        textView.text = e.toString()
-        setContentView(textView)
+    override fun onPause() {
+        super.onPause()
+        glView.onPause()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        glView.onResume()
     }
 }
