31973433bca89c813b03b361fc7cb248eb12bec1
Ninhoplayer6gg <filhofilho281@gmail.com>
2026-03-01 22:21:58 -0300

    Add TextView to MainActivity with startup message

--- app/src/main/java/com/arkanis/engine/MainActivity.kt
+++ app/src/main/java/com/arkanis/engine/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.arkanis.engine
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val textView = TextView(this)
+        textView.text = "Arkanis Engine Iniciada 🚀"
+        textView.textSize = 24f
+
+        setContentView(textView)
     }
 }
